0aa84265f2aacad4ff1be9d099afc08a67310772
Jayvee Ramos <jayvee@ag-investment.net>
2020-08-17 13:19:43 +0800

    -problem

--- src/com/solve/Main.java
+++ src/com/solve/Main.java
@@ -1,8 +1,13 @@
 package com.solve;
 
+import com.solve.partOne.LeetCodeOne;
+
 public class Main {
+    private static final LeetCodeOne leetCodeOne = new LeetCodeOne();
 
     public static void main(String[] args) {
-	// write your code here
+        int[] input = {3, 0, 1};
+
+        System.out.print(leetCodeOne.missingNumber(input));
     }
 }
